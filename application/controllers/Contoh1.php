1e2fc5c19856da9bf521351dbe63b39b576417eb
ptrrhy <ttatae4@gmail.com>
2023-09-26 20:33:38 +0700

    Membuat project CI Pustaka Booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+  public function index(){
+    echo "<h1> perkenalkan </h1>";
+    echo "Nama saya Starmonth
+    saya tinggal di daerah Mars
+    olahraga yang saya sukai adalah Pencak Silat";
+  }
+
+}  
+?>
